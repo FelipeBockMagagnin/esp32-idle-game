e59f4f120912166da2c1aa1818025ecf56d3820b
Felipe Magagnin <felipebmagagnin@gmail.com>
2026-09-21 21:39:55 -0300

    feat: add upgrade screen and game config

--- src/game/GameConfig.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameConfig.h"
-
-const UpgradeDef UPGRADES[UPGRADE_COUNT] = {
-    {"Pickaxe", OreEnum::COPPER, 100, {10, 0, 0}, 115},
-    {"Minecart", OreEnum::COPPER, 1000, {100, 0, 0}, 115},
-    {"Drill", OreEnum::SILVER, 100, {500, 0, 0}, 120},
-    {"Excavator", OreEnum::GOLD, 100, {2000, 50, 0}, 125},
-};
